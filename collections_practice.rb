9acd03c34856a84291295303fbec5e68af639c56
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-08 17:17:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,4 +14,13 @@ end
   
 def sort_array_char_count(arr)
   
+  arr.sort do |a, b|
+  if a.length == b.length
+    0
+  elsif a.length < b.length
+    -1
+  elsif a.length > b.length
+    1
+  end
+end
 end 
